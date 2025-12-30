c340914be228bf8f04dee43dd9843fe9f154deca
Shanib <shanibshanu356@gmail.com>
2025-12-30 10:56:20 +0530

    Add error and info logs for job-related operations on 2025-12-30
    - Created error log for failed retrieval of saved jobs due to missing 'salary_currency' column.
    - Added info logs for personalized job requests, job generation, and job application submission, including relevant user data.

--- obj/Debug/net9.0/MyFirstApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyFirstApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f921ab93b8b2a385d5937d5b89f75034c99fa4f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2423d0b2351d46916bad6a3dc44f1604e0b1258d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
