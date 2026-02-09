ea5f7ba33710d8a7fcb08d02815eb31e7749e67a
Shanib <shanibshanu356@gmail.com>
2026-02-09 20:49:56 +0530

    fix alignment

--- obj/Debug/net9.0/MyFirstApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyFirstApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edee46f22e4ce7713a633be9b0e4a459b6df8f9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77a0779dd59c2243a6bc353802d0482f9b34d554")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
